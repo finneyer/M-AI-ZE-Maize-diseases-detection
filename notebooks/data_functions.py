ec39a4e5b24f6a52d57f9d677c4ddf259fc692b0
Finn Eyer <finn.eyer@stud.hslu.ch>
2025-04-20 10:01:49 +0000

    [BUGFIX] Substituted constant with function parameter

--- notebooks/data_functions.py
+++ notebooks/data_functions.py
@@ -34,3 +34,36 @@ def clip_negative_coord_values(df):
     cols = ['x1', 'y1', 'x2', 'y2']
     df[cols] = df[cols].clip(lower=0)
     return df
+
+def resize_images(input_folder, output_folder, new_size: tuple):
+    os.makedirs(output_folder, exist_ok=True)
+
+    for filename in os.listdir(input_folder):
+        if filename.lower().endswith('.jpg'):
+            img_path = os.path.join(input_folder, filename)
+            img = Image.open(img_path)
+            img_resized = img.resize(new_size)
+            img_resized.save(os.path.join(output_folder, filename))
+
+def get_image_size_dict(folder_path):
+    image_size_dict = {}
+    for filename in os.listdir(folder_path):
+        if filename.lower().endswith('.jpg'):
+            img = Image.open(os.path.join(folder_path, filename))
+            image_size_dict[filename] = img.size
+
+    return image_size_dict
+
+def add_image_size_to_df(df, image_size_dict):
+    df = df.copy()
+    df['original_width'] = df['image'].map(lambda img: image_size_dict[img][0])
+    df['original_height'] = df['image'].map(lambda img: image_size_dict[img][1])
+    return df
+
+def copy_imgs_to_folder(df, dst_folder, org_img_folder_path):
+    for index, row in df.iterrows():
+        img_filename = row['image']
+        img_type = row['type']
+        src_path = org_img_folder_path + '/images_' + img_type
+        
+        shutil.copy(src_path, dst_folder + '/' + image)
